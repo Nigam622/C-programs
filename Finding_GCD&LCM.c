ec20a3da6d916cc4f4d6645706ad0adcd55e40d1
NIGAM.N <80585462+Nigam622@users.noreply.github.com>
2022-05-18 14:43:20 +0530

    Create  Finding_GCD&LCM.c
    Common program for finding GCD & LCM.

---
+++ Finding_GCD&LCM.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+    
+    int num1,num2,i,gcd,lcm;                        
+    printf("Enter the two numbers:\n");
+    scanf("%d%d",&num1,&num2);
+    
+    for(i=1;i<=num1&&i<=num2;i++)
+    {
+        if(num1%i==0&&num2%i==0)
+        gcd=i;
+    }
+    lcm=(num1*num2)/gcd;
+    
+      printf("GCD=%d\n",gcd);
+       printf("LCM=%d\n",lcm);
